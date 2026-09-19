7bd6c9a205e1bd3c4b597f93c0ecc414c87dfb50
Jordan Mensah <koricam42@gmail.com>
2026-09-19 15:49:35 -0400

    Created starting project files and added required docstring headers

---
+++ Lab1_koricam42-2.py
@@ -0,0 +1,12 @@
+"""
+Program name: Geometry Calculator - main
+Author: Jordan Mensah
+Purpose: This program calculates area and perimeter or circumference of a rectangles and circles via imported functions.
+Starter code: N/A
+Date: 9/17/26
+"""
+
+# Aliases import explanation (TBD)
+
+import rectangle 
+import circle 
---
+++ rectangle.py
@@ -0,0 +1,7 @@
+"""
+Program name: Geometry Calculator - rectangle.py
+Author: Jordan Mensah
+Purpose: 
+Starter code: N/A
+Date: 9/17/26
+"""
