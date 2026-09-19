b741fd82f61e7969d39cf7bce6149f26abe43542
Jordan Mensah <koricam42@gmail.com>
2026-09-19 17:03:49 -0400

    Added main menu while loop and basic user input placeholders

--- Lab1_koricam42-2.py
+++ Lab1_koricam42-2.py
@@ -10,3 +10,31 @@ Date: 9/17/26
 
 import rectangle 
 import circle 
+
+# I modeled the print statements to match the output of the examples in the lab instuctions
+# But I'm not sure if the print output (of the non-math sections) has to match the example perfectly
+def main():
+    while True:
+        print("Geometry Calculator")
+        print("1. Calculate Circle Area")
+        print("2. Calculate Circle Circumference")
+        print("3. Calculate Rectangle Area")
+        print("4. Calculate Rectangle Perimeter")
+        print("5. Exit")     
+
+        user_input = input("Select and option (1-5): ")
+
+# Placeholders for 1-4
+        if user_input == "1":
+            pass
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            pass
+        elif user_input == "4":
+            pass
+        elif user_input == "5":
+            print("Goodbye.")
+            break
+        else:
+            print("Invalid input. Options are only 1-5. Please try again.:")
