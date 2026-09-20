cf1869c2d8d5c6f15525296aac51e1ce82296304
Jordan Mensah <koricam42@gmail.com>
2026-09-20 12:06:57 -0400

    Implement rectangle math functions with proper type hints and docstrings

--- rectangle.py
+++ rectangle.py
@@ -5,3 +5,31 @@ Purpose: Calculate the area and perimeter of a rectangle
 Starter code: N/A
 Date: 9/17/26
 """
+
+
+def calc_area(width: float, height: float) -> float:
+    """Calculates the area of a rectangle
+
+    Args:
+        width (float): The width of a rectangle
+        height (float): The height of a rectangle
+
+    Returns:
+        float: The area of a rectangle
+    """
+    area = width * height
+    return area
+
+def calc_perimeter(width: float, height: float) -> float:
+    """Calculates the perimeter of a rectangle
+
+    Args:
+        width (float): The width of a rectangle
+        height (float): The height of a rectangle
+
+    Returns:
+        float: The perimeter of a rectangle
+    """
+    perimeter = 2 * (height + width)
+    
+    return perimeter
