eb2a7697ca202ca90a7e1dad2533ec852cc7733a
Jordan Mensah <koricam42@gmail.com>
2026-09-20 21:18:41 -0400

    Add last docstrings and type hints to main functions

--- Lab1_koricam42-2.py
+++ Lab1_koricam42-2.py
@@ -14,15 +14,24 @@ from rectangle import calc_area as rectangle_area, calc_perimeter
 from circle import calc_area as circle_area, calc_circumference
 
 # I modeled the print statements to match the output of the examples in the lab instuctions
-# But I'm not sure if the print output (of the non-math sections) has to match the example perfectly
-def error_handler(user_text):
+# But I'm not sure if the print output formatting (of the non-math sections) has to match the example perfectly
+def error_handler(user_text: str) -> float:
+    """Function to safely handle invalid user input to prevent crashing.
+
+    Args:
+        user_text (str): Text to prompt user for input.
+
+    Returns:
+        float: valid decimal number input from the user.
+    """
     while True:
         try:
             true_input = float(input(user_text))
             return true_input
         except ValueError:
             print("Invalid input. Please enter a number.")
-def main():
+def main() -> None:
+    """Runs main menu loop for program, handles user inputs to calculate and display measurements"""
     while True:
         print("\nGeometry Calculator")
         print("-------------------")
@@ -43,16 +52,19 @@ def main():
             radius = error_handler("Enter the radius of the circle: ")
             circumference = calc_circumference(radius)
             print(f'\nThe circumference of the circle is: {circumference}')
+            input("Press Enter to continue.")
         elif user_input == "3":
             width = error_handler("Enter the width of the rectangle: ")
             height = error_handler("Enter the height of the rectangle: ")
             area = rectangle_area(width, height)
             print (f'\nThe area of the rectangle is: {area}')
+            input("Press Enter to continue.")
         elif user_input == "4":
             width = error_handler("Enter the width of the rectangle: ")
             height = error_handler("Enter the height of the rectangle: ")
             perimeter = calc_perimeter(width, height)
             print (f'\nThe perimeter of the rectangle is: {perimeter}')
+            input("Press Enter to continue.")
         elif user_input == "5":
             print("\nGoodbye.")
             break
