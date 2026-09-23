faac29ff7de4fdc2d8ef2894289d42ec75a082e8
Jordan Mensah <koricam42@gmail.com>
2026-09-23 02:15:16 -0400

    Add placholder methods for player file

--- Lab 2/player.py
+++ Lab 2/player.py
@@ -5,3 +5,23 @@ Purpose: Represents the player with a name, wallet of coins, and a coin object
 Starter Code: N/A
 Date: 9/21/26
 """
+
+from coin import Coin
+
+class Player():
+    
+    def __init__(name):
+
+        pass
+    def toss_coin(self):
+        pass
+    def get_coin_side(self):
+        return self.get_coin_side()
+    def win_coin(self):
+        pass
+    def lose_coin(self):
+        pass
+    def get_wallet(self):
+        pass
+    def get_name(self):
+        pass
