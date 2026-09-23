c8a563136d9aabd408f8fa418d7bb085599f3b45
Jordan Mensah <koricam42@gmail.com>
2026-09-23 17:20:41 -0400

    Build placeholder for main lab file

--- Lab 2/Lab9_koricam42-1.py
+++ Lab 2/Lab9_koricam42-1.py
@@ -5,3 +5,12 @@ Purpose: Runs main game loop, creates the objects, and manages the rules/winners
 Starter Code: N/A
 Date: 9/21/26
 """
+
+from player import Player
+
+def main():
+
+    pass
+
+
+main()
