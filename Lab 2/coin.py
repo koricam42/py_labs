c2744b668c76be811c59f5e93d81e8f111ca3cb8
Jordan Mensah <koricam42@gmail.com>
2026-09-22 22:35:25 -0400

    Add required methods for coin function with pass placeholders

--- Lab 2/coin.py
+++ Lab 2/coin.py
@@ -5,3 +5,11 @@ Purpose: Represents a single coin with a private state of either "Heads" or "Tai
 Starter Code: N/A
 Date: 9/21/26
 """
+
+class Coin():
+    def __init__():
+        pass
+    def toss():
+        pass
+    def get_sideup():
+        pass
