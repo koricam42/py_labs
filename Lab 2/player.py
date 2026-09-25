1603a3a224f65b8221e94c52802ca27dec709440
Jordan Mensah <koricam42@gmail.com>
2026-09-25 18:24:05 -0400

    Added docstrings and typehints to player.py

--- Lab 2/player.py
+++ Lab 2/player.py
@@ -9,21 +9,48 @@ Date: 9/21/26
 from coin import Coin
 
 class Player():
-    
-    def __init__(self, name):
+    """Class that represents a player in the game
+    """
+    def __init__(self, name: str) -> None:
+        """Initializes the player with a name, 20-coin wallet, and Coin object
+
+        Args:
+            name (str): Name of player
+        """
         self.__name = name
         self.__wallet = 20
         self.__coin= Coin()
 
-    def toss_coin(self):
+    def toss_coin(self) -> None:
+        """Tosses the players coin
+        """
         self.__coin.toss()
-    def get_coin_side(self):
+    def get_coin_side(self) ->  str:
+        """Returns the facing up side of the players coin
+
+        Returns:
+            str: Side of the coin facing up
+        """
         return self.__coin.get_sideup()
-    def win_coin(self):
+    def win_coin(self) -> None:
+        """Adds a coin to the players wallet
+        """
         self.__wallet += 1
-    def lose_coin(self):
+    def lose_coin(self) -> None:
+        """Removes a coin from the players wallet
+        """
         self.__wallet -= 1
-    def get_wallet(self):
+    def get_wallet(self) -> int:
+        """Returns the current number of coins in the players wallet
+
+        Returns:
+            int: The current number of coins
+        """
         return self.__wallet
-    def get_name(self):
+    def get_name(self) -> str:
+        """Returns the player name
+
+        Returns:
+            str: Player name
+        """
         return self.__name
