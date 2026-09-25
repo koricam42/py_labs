f3e6eebbe162115652be959f1aa817cc3e15f123
Jordan Mensah <koricam42@gmail.com>
2026-09-25 11:50:34 -0400

    Create two player objects and formed main while loop to handle user input

--- Lab 2/Lab9_koricam42-1.py
+++ Lab 2/Lab9_koricam42-1.py
@@ -9,8 +9,18 @@ Date: 9/21/26
 from player import Player
 
 def main():
+    player1 = Player("Player 1")
+    player2 = Player("Player 2")
 
-    pass
+    while True:
+        player_choice = input("Do you want to play? Press 'Y' or 'y' to continue")
+        
+        if player_choice == 'Y' or player_choice == 'y':
+            pass
+        else:
+            print("Incorrect option.")
+
+
 
 
 main()
