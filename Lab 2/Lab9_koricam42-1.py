55b856623febd9fd29543bb81f60bde2918b5cb4
Jordan Mensah <koricam42@gmail.com>
2026-09-25 15:07:30 -0400

    Rework print statement order flow, implement coin toss mechanic, and add current score display

--- Lab 2/Lab9_koricam42-1.py
+++ Lab 2/Lab9_koricam42-1.py
@@ -13,12 +13,34 @@ def main():
     player2 = Player("Player 2")
 
     while True:
-        player_choice = input("Do you want to play? Press 'Y' or 'y' to continue")
+        print("\nMatch Coin Game")
+        player_choice = input("Do you want to play the Match Coins Game? Input 'Y' or 'y' to continue")
         
         if player_choice == 'Y' or player_choice == 'y':
-            pass
-        else:
-            print("Incorrect option.")
+            print(f"Player 1 has {player1.get_wallet()} coins")
+            print(f"Player 2 has {player2.get_wallet()} coins")
+            print()
+
+            player1.toss_coin()
+            player2.toss_coin()
+
+            p1_side = player1.get_coin_side()
+            p2_side = player2.get_coin_side()
+
+
+            print(f'Player 1 tossed {p1_side}')
+            print(f'Player 2 tossed {p2_side}')
+
+            if p1_side == p2_side:
+                pass
+            else:
+                pass
+                
+        elif player_choice == "N" or player_choice == "n":
+            print("See you later.")
+            break
+        else:  
+            print("Invalid input.")
 
 
 
